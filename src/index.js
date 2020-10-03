51169b67a814c7d8b44936991739660dba49bba6
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-10-03 16:07:46 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 function generateCodeFromObject(obj){
     //return a code generated string
+    return "string";
    }
    
 module.exports=generateCodeFromObject;
 
 
-// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
