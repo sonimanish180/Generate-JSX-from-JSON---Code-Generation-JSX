4dadfce35456d0dc4274dffd9748d16338075448
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-10-04 14:43:37 +0530

    Vanilla js 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,43 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
+  render() {
+    let app = document.getElementById("root");
+    let clock = document.createElement("Clock");
 
-        return(
-            <>
-            <Clock root = "true" style = {{display : "flex", flexDirection : "row", justifyContent : "center"}}>
-                <Hour/>                
-                <Minute style = {{color : "green", fontSize : 30}}>Make Changes</Minute>
-            </Clock>
-               
-            </>
-        )
-    }
+    clock.setAttribute(("root", "true"));
+    clock.style.display = "flex";
+    clock.style.flexDirection = "row";
+    clock.style.justifyContent = "center";
+
+    app.appendChild(clock);
+
+    let hour = document.createElementFragment("Hour");
+
+    let minute = document.createElement("Minute");
+
+    minute.style.color = "green";
+    minute.style.fontSize = 30;
+
+    clock.appendChild(hour);
+    clock.appendChild(minute);
+
+    return (
+      <>
+        {clock};
+        {/* <Clock
+          root="true"
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center"
+          }}
+        >
+          <Hour />
+          <Minute style={{ color: "green", fontSize: 30 }}></Minute>
+        </Clock> */}
+      </>
+    );
+  }
 }
-
-
-export default App;
-
-
